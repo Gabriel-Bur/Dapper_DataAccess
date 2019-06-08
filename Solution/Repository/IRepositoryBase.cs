1d66d62d4070748349b057a275b78fe7e9dc7fa5
Gabriel Bur <gabriel.bur@outlook.com>
2019-06-07 23:33:11 -0300

    First commit

---
+++ Solution/Repository/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+
+namespace Repository
+{
+    public interface IRepositoryBase<TEntity> where TEntity : BaseEntity
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(long id);
+        long Insert(TEntity entity);
+        bool Delete(long id);
+
+    }
+}
---
+++ Solution/Repository/RepositoryBase.cs
@@ -0,0 +1,56 @@
+﻿using Domain.Entities;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper.Contrib.Extensions;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly string _ConnectionString;
+
+        protected RepositoryBase()
+        {
+            _ConnectionString = ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString;
+        }
+
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+           using(var db = new SqlConnection(_ConnectionString))
+           {
+                return db.GetAll<TEntity>();
+           }
+        }
+
+        public virtual TEntity GetById(long id)
+        {
+            using(var db = new SqlConnection(_ConnectionString))
+            {
+                return db.Get<TEntity>(id);
+            }
+        }
+
+        public virtual long Insert(TEntity entity)
+        {
+            using(var db = new SqlConnection(_ConnectionString))
+            {
+                return db.Insert<TEntity>(entity);
+            }
+        }
+
+        public virtual bool Delete(long id)
+        {
+            using (var db = new SqlConnection(_ConnectionString))
+            {
+                var entity = GetById(id);
+
+                if (entity == null) return false;
+
+                return db.Delete(entity);
+            }
+        }
+
+    }
+}
